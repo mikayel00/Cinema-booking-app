31035802a1be7d92227c579051b0ea7caaf27697
artaksafaryan <artaksafaryan20002017@gmail.com>
2022-02-02 14:50:56 +0400

    Made menu responsible

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function toggleMenu() {
+    let menuItem = document.getElementById("menu__list");
+    if (menuItem.style.display === "none") {
+        menuItem.style.display = "flex";
+    } else {
+        menuItem.style.display = "none";
+    }
+}
+
+let menu_icon = document.getElementById('nav_icon1');
+
+menu_icon.addEventListener('click', toggleMenu);
